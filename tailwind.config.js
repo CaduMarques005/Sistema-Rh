279e149ce62e93470b5086231c18aee97238a74e
CaduMarques005 <cadugoleiro12@gmail.com>
2024-06-24 19:38:10 -0300

    testando husky

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
+import flowbitePlugin from 'flowbite/plugin'
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
+        './src/**/*.{html,js}', // ajuste conforme necessário
+        './public/index.html',
+        './node_modules/flowbite/**/*.js',
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
@@ -17,5 +21,8 @@ export default {
         },
     },
 
-    plugins: [forms],
+    plugins: [
+        forms,
+        flowbitePlugin,
+    ],
 };
